8b6d96a2daebf3bb3c2bbdc270e5d3455acd708c
PedroPinto23 <pdrpdr.ph@gmail.com>
2019-08-24 14:14:55 -0300

    melhorado

--- src/App.js
+++ src/App.js
@@ -1,52 +1,54 @@
 import React from "react";
-import axios from "axios";
-
-const LINK =
-  "https://0o7qeyq4of.execute-api.us-east-2.amazonaws.com/dev/radiocidade/";
 
 export default class App extends React.Component {
-  state = {
-    playlist: [],
-    album: ""
-  };
-  componentDidMount() {
-    this.getPlaylist();
-    this.postPlaylist()
+  constructor() {
+    super();
+    this.state = {
+      playList: []
+    };
   }
+  componentDidMount() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.open(
+      "GET",
+      "https://0o7qeyq4of.execute-api.us-east-2.amazonaws.com/dev/radiocidade/teste-front",
+      true
+    );
 
-  postPlaylist = async () => {
-    
-    const res = await fetch(`${LINK}capas`, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ musica: "Easy", artista: "Faith no More" })
-    });
+    xhttp.onreadystatechange = () => {
+      if (xhttp.readyState === 4 && xhttp.status === 200) {
+        console.log("ok");
+        this.setState({
+          playList: JSON.parse(xhttp.responseText)
+        });
+        this.setState({
+          playList: this.state.playList.body
+        });
+        console.log(this.state.playList);
 
-    const json = await res.json();
-    this.setState({ album: json.capa });
-    console.log(this.state.album);
-  };
-  getPlaylist = async () => {
-    const res = await fetch(`${LINK}teste-front`, {
-      method: "GET"
-    });
-    const json = await res.json();
-    this.setState({ playlist: json.body });
-    console.log(this.state.playlist);
-  };
+        var xhttp2 = new XMLHttpRequest();
+        xhttp2.onreadystatechange = () => {
+          if (xhttp2.readyState === 4 && xhttp2.status === 200) {
+            var obj2 = JSON.parse(xhttp2.responseText);
+
+            console.log(obj2);
+          }
+        };
+        xhttp2.open(
+          "POST",
+          "https://0o7qeyq4of.execute-api.us-east-2.amazonaws.com/dev/radiocidade/capas",
+          true
+        );
+        xhttp2.send();
+      }
+    };
+
+    xhttp.send();
+  }
   render() {
     return (
-      <div className = 'container-fluid'>
-        {this.state.playlist.map((a,key) => {
-          return (
-            <div className = 'text-primary' key = {key + 1}>
-              <p>{a.artista}</p>
-              <h3>{a.musica}</h3>
-              <br />
-              <br />
-            </div>
-          );
-        })}
+      <div>
+        
       </div>
     );
   }
