6a0b73f08f02e145301a4608d43c8459d61bc121
PedroPinto23 <pdrpdr.ph@gmail.com>
2019-08-26 03:16:06 -0300

    quase-la

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
+import "./index.css";
 
 export default class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      playList: []
+      playList: [],
+      abums: []
     };
   }
   componentDidMount() {
+    var i = 0;
     var xhttp = new XMLHttpRequest();
     xhttp.open(
       "GET",
@@ -17,30 +20,34 @@ export default class App extends React.Component {
 
     xhttp.onreadystatechange = () => {
       if (xhttp.readyState === 4 && xhttp.status === 200) {
-        console.log("ok");
-        this.setState({
-          playList: JSON.parse(xhttp.responseText)
-        });
-        this.setState({
-          playList: this.state.playList.body
-        });
-        console.log(this.state.playList);
+        var obj = JSON.parse(xhttp.responseText);
 
-        var xhttp2 = new XMLHttpRequest();
-        xhttp2.onreadystatechange = () => {
-          if (xhttp2.readyState === 4 && xhttp2.status === 200) {
-            var obj2 = JSON.parse(xhttp2.responseText);
-
-            console.log(obj2);
-          }
-        };
-        xhttp2.open(
-          "POST",
-          "https://0o7qeyq4of.execute-api.us-east-2.amazonaws.com/dev/radiocidade/capas",
-          true
-        );
-        xhttp2.send();
+        this.setState({
+          playList: obj.body
+        });
+        var playList = this.state.playList;
+        var obj2 = JSON.stringify(playList[0]);
+        console.log(obj2);
       }
+
+      var xhttp2 = new XMLHttpRequest();
+      xhttp2.open(
+        "POST",
+        "https://0o7qeyq4of.execute-api.us-east-2.amazonaws.com/dev/radiocidade/capas",
+        true
+      );
+      xhttp2.setRequestHeader("Content-type", "application/json");
+
+      xhttp2.onreadystatechange = () => {
+        if (xhttp2.readyState === 4 && xhttp2.status === 200) {
+          var obj3 = JSON.parse(xhttp2.responseText);
+          console.log(obj3);
+        }
+        this.setState({ albums: obj3 });
+        var album = this.state.albums;
+        console.log(album)
+      };
+      xhttp2.send(obj2);
     };
 
     xhttp.send();
@@ -48,7 +55,33 @@ export default class App extends React.Component {
   render() {
     return (
       <div>
-        
+        <div className="container-fluid">
+          {this.state.playList.map((m, key) => {
+            return (
+              <div key={key + 1} className="col-lg-3">
+                <img
+                  src="faith-no-more_-_easy.jpg"
+                  className="img-fluid"
+                  alt=""
+                  width="100%"
+                  height="auto"
+                ></img>
+                <table className="table table-inverse table-dark">
+                  <thead>
+                    <tr>
+                      <th className="text-center">{m.artista}</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td className="text-center">{m.musica}</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            );
+          })}
+        </div>
       </div>
     );
   }
